e56f12ad5ed2d2cd3e7e3c24de185c4fff862f3c
Matt Harden <matt.harden@gmail.com>
2017-02-06 01:42:37 +0000

    problem 44 solution

---
+++ problem44/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func pent(i int) int {
+	return (3*i*i - i) >> 1
+}
+
+func invPent(x int) (int, bool) {
+	var i int
+	f := (1 + math.Sqrt(1+24*float64(x))) / 6
+	i = int(f)
+	return i, pent(i) == x
+}
+
+// Pentagonal numbers: 1, 5, 12, 22, 35, 51, 70
+// diff:                 4, 7, 10, 13, 16, 19
+
+func main() {
+	for i, dp, p := 1, 1, 1; i < 1<<32; i, dp, p = i+1, dp+3, p+dp+3 {
+		for j, dq, q := 1, 1, 1; dq <= p; j, dq, q = j+1, dq+3, q+dq+3 {
+			if _, ok := invPent(q + p); ok {
+				// fmt.Printf("\r%16d %16d", p, q+p)
+				if _, ok := invPent(q + q + p); ok {
+					fmt.Println(p)
+					return
+				}
+			}
+		}
+	}
+}
