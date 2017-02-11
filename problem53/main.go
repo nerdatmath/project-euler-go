4b04c223a36813745b3d5cb366196d90302f46ca
Matt Harden <matt.harden@gmail.com>
2017-02-11 05:30:36 +0000

    problem53

--- problem53/main.go
+++ problem53/main.go
@@ -7,23 +7,25 @@ func main() {
 	count := 0
 	n, r, nCr := 0, 0, 1
 	for n <= 100 {
+		// loop invariant: nCr == count(n, r)
 		// loop invariant: nCr <= target
-		// loop invariant: 0 <= r <= n - r <= n <= 100
-		if r < n-r-1 {
+		// loop invariant: 0 <= r <= n/2
+		// loop invariant: r (and therefore nCr) are maximized
+		// invariant: nCr > target or r == n/2
+		if n-r-r-1 > 0 {
+			// nCx > target for all r+1 <= x <= n-r-1
+			count += n - r - r - 1
+		}
+		// Increase n, which may take nCr over the target
+		n++
+		nCr *= n
+		nCr /= n - r
+		if r < n-r-1 && nCr <= target {
 			// Increase r
 			nCr *= n - r
 			r++
 			nCr /= r
 		}
-		if nCr > target {
-			// We just stepped over the boundary
-			// nCx > target for all r <= x <= n-r
-			count += n - r - r + 1
-		}
-		// Increase n
-		n++
-		nCr *= n
-		nCr /= n - r
 		// Decrease r while nCr > target
 		for nCr > target {
 			nCr *= r
